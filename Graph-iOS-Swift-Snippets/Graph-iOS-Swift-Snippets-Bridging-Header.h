54488d5fdf28235d2677f28630d410729fb0da4e
Jason Kim <Jason@Jasons-MBP.home>
2016-07-08 17:00:24 -0700

    update project settings

--- Graph-iOS-Swift-Snippets/Graph-iOS-Swift-Snippets-Bridging-Header.h
+++ Graph-iOS-Swift-Snippets/Graph-iOS-Swift-Snippets-Bridging-Header.h
@@ -8,3 +8,4 @@
 
 #import <MSGraphSDK.h>
 #import <MSGraphSDKNXOAuth2.h>
+#import <MSAuthConstants.h>
---
+++ Graph-iOS-Swift-SnippetsTests/Graph_iOS_Swift_SnippetsTests.swift
@@ -0,0 +1,38 @@
+//
+//  Graph_iOS_Swift_SnippetsTests.swift
+//  Graph-iOS-Swift-SnippetsTests
+//
+//  Created by Jason Kim on 7/8/16.
+//  Copyright © 2016 Jason Kim. All rights reserved.
+//
+
+import XCTest
+
+@testable import Graph_iOS_Swift_Snippets
+
+class Graph_iOS_Swift_SnippetsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let _ = MS_AADV2_TOKEN_TYPE
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
